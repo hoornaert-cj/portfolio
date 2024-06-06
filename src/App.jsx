aeb2180bb9209cfe3d56e041a9ff88b764095598
hoornaert-cj <97850176+hoornaert-cj@users.noreply.github.com>
2024-06-06 15:11:23 -0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import { Route, Routes, Link, NavLink } from 'react-router-dom'
+import Home from './templates/Home'
+import About from './templates/About'
+import Posts from './templates/Posts'
+import Post from './templates/Post'
+import Services from './templates/Services'
+
+function App() {
+
+  return (
+    <>
+      <header id="masthead" className="site-header">
+        <div className="site-branding">
+          <p className="site-title">Headless WordPress App</p>
+        </div>
+        <nav className="site-navigation">
+          <ul>
+            <li><NavLink to='/' end>Home</NavLink></li>
+            <li><NavLink to='/about'>About</NavLink></li>
+            <li><NavLink to='/blog'>Blog</NavLink></li>
+            <li><NavLink to='/services'>Services</NavLink></li>
+          </ul>
+        </nav>
+      </header>
+      <main id="main">
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/blog' element={<Posts />} />
+          <Route path='/blog/:slug' element={<Post />} />
+          <Route path='/services' element={<Services />} />
+        </Routes>
+      </main>
+      <footer>
+        <nav className="site-navigation">
+          <ul>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/about'>About</Link></li>
+          </ul>
+        </nav>
+        <p className="copyright">Created for <a href="https://wp.bcitwebdeveloper.ca/" target="_blank" rel="noopener noreferrer">FWDP 3600</a>.</p>
+      </footer>
+    </>
+  )
+}
+
+export default App
---
+++ src/utilities/Utilities.jsx
@@ -0,0 +1,17 @@
+export const restBase = 'https://wp.bcitwebdeveloper.ca/mindset-demo/wp-json/wp/v2/'
+
+export const featuredImage = ( featuredImageObject ) => {
+  let imgWidth = featuredImageObject.media_details.sizes.full.width;
+  let imgHeight = featuredImageObject.media_details.sizes.full.height;
+  let imgURL = featuredImageObject.source_url;
+  let img = `<img src="${imgURL}" 
+      width="${imgWidth}"
+      height="${imgHeight}"
+      alt="${featuredImageObject.alt_text}"
+      srcset="${imgURL} ${imgWidth}w,
+      ${featuredImageObject.media_details.sizes.large ? featuredImageObject.media_details.sizes.large.source_url + ' 1024w,' : ''}
+      ${featuredImageObject.media_details.sizes.medium_large ? featuredImageObject.media_details.sizes.medium_large.source_url + ' 768w,' : ''}
+      ${featuredImageObject.media_details.sizes.medium ? featuredImageObject.media_details.sizes.medium.source_url + ' 300w' : ''}"
+      sizes="(max-width: ${imgWidth}) 100vw, ${imgWidth}px">`;
+  return {__html: img}
+}

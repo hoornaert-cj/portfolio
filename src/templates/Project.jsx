9af3277584fb9c63717db5ac0cfc3bb1295d2c7f
hoornaert-cj <97850176+hoornaert-cj@users.noreply.github.com>
2024-06-11 16:53:01 -0700

    fixed animations and updated individual project pages.

--- src/templates/Project.jsx
+++ src/templates/Project.jsx
@@ -5,12 +5,10 @@ import { restBase } from '../utilities/Utilities';
 
 const Project = () => {
   const { id } = useParams();
-  const restPath = `${restBase}cjh-project/${id}`;
+  const restPath = `${restBase}cjh-project/${id}/?acf_format=standard`;
   const [projectData, setProjectData] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [error, setError] = useState(null);
-  const [projectAnimation, setProjectAnimation] = useState(null);
-
 
   useEffect(() => {
     const fetchProject = async () => {
@@ -21,18 +19,7 @@ const Project = () => {
           throw new Error(`HTTP error! Status: ${response.status}`);
         }
         const data = await response.json();
-        console.log('Fetched project data:', data); // Log the fetched data
-
-        // Fetch media details if project_animation is present
-        if (restPath.acf && restPath.acf.animation_project) {
-          const mediaResponse = await fetch(`https://chrishoornaert.com/securepanel/wp-json/wp/v2/media/${data.acf.animation_project}`);
-          if (!mediaResponse.ok) {
-            throw new Error(`HTTP error! Status: ${mediaResponse.status}`);
-          }
-          const mediaData = await mediaResponse.json();
-          console.log('Fetched media data:', mediaData); // Log the fetched media data
-          setProjectAnimation(mediaData); // Set the projectAnimation state
-        }
+        console.log('Fetched project data:', data);
 
         setProjectData(data);
         setIsLoaded(true);
@@ -45,8 +32,6 @@ const Project = () => {
     fetchProject();
   }, [restPath]);
 
-
-
   if (!isLoaded) {
     return <Loading />;
   }
@@ -55,25 +40,27 @@ const Project = () => {
     return <div className="error-message">Error fetching project: {error}</div>;
   }
 
-  console.log('Project Animation:', projectAnimation);
-
   return (
     <div className="project-details">
-      <h1>{projectData.acf.indv_project_heading}</h1>
-      {projectAnimation && (
-  <img
-    src={projectAnimation.source_url}
-    alt={projectData.acf.project_animation_alt || "Project animation"}
-  />
-)}
-      {projectData.acf.description_repeater && (
-        <div className="description-repeater">
-          <h3>{projectData.acf.indv_project_heading}</h3>
-          {projectData.acf.description_repeater.map((desc, index) => (
-            <div key={index} dangerouslySetInnerHTML={{ __html: desc.indv_project_description }} />
-          ))}
-        </div>
+      <h1>{projectData.acf.indv_project_title_heading}</h1>
+      {projectData.acf.project_animation && (
+        <img
+          src={projectData.acf.project_animation.url}
+          alt={projectData.acf.project_animation.alt || "Project animation"}
+          autoPlay
+          style={{ width: "400px" }}
+        />
       )}
+{projectData.acf.description_repeater && (
+  <div className="description-repeater">
+    {projectData.acf.description_repeater.map((desc, index) => (
+      <div key={index}>
+        <h3>{desc.indv_project_type}</h3>
+        <div dangerouslySetInnerHTML={{ __html: desc.indv_project_description }} />
+      </div>
+    ))}
+  </div>
+)}
       {projectData.acf.tools_used_repeater && (
         <div className="tools-used-repeater">
           {projectData.acf.tools_used_repeater.map((tool, index) => (

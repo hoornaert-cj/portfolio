891980641a836d1f74f90bf516224b4a14723c57
hoornaert-cj <97850176+hoornaert-cj@users.noreply.github.com>
2024-06-17 07:41:19 -0700

    Changet the Skills tabs to React-tabs and added styling to the Skills page

--- src/templates/Skills.jsx
+++ src/templates/Skills.jsx
@@ -1,13 +1,14 @@
-import { useState, useEffect } from 'react';
-import Loading from '../utilities/Loading';
-import { restBase } from '../utilities/Utilities';
-import GlobalButtons from '../components/GlobalButtons';
+import React, { useState, useEffect } from "react";
+import Loading from "../utilities/Loading";
+import { restBase } from "../utilities/Utilities";
+import GlobalButtons from "../components/GlobalButtons";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
 
-const Services = () => {
-  const restPath = restBase + 'pages/81';
+const Skills = () => {
+  const restPath = restBase + "pages/81?acf_format=standard";
   const [restData, setRestData] = useState([]);
   const [isLoaded, setLoadStatus] = useState(false);
-  const[selectedTab, setSelectedTab] = useState('all');
 
   useEffect(() => {
     const fetchData = async () => {
@@ -22,7 +23,7 @@ const Services = () => {
           setLoadStatus(false);
         }
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
         setLoadStatus(false);
       }
     };
@@ -31,44 +32,112 @@ const Services = () => {
 
   const filterIcons = (category) => {
     if (!restData.acf || !restData.acf.skills_icon_section) return [];
-    if (category === 'all') return restData.acf.skills_icon_section;
-    return restData.acf.skills_icon_section.filter(skill => skill.skills_category === category);
+    if (category === "all") return restData.acf.skills_icon_section;
+    return restData.acf.skills_icon_section.filter(
+      (skill) => skill.skills_category === category,
+    );
   };
-
 
   return (
     <>
       {isLoaded ? (
         <section id="skills" className="skills-wrapper">
-          <article className='skills-intro'>
-            <h2>{restData.acf.skills_header}</h2>
-            <div dangerouslySetInnerHTML={{ __html: restData.acf.skills_intro }} />
-          </article>
-          <section className='skills-icons'>
-            <div className="tabs">
-              <button onClick={() => setSelectedTab('all')}>All</button>
-              <button onClick={() => setSelectedTab('Web Dev')}>Web Dev</button>
-              <button onClick={() => setSelectedTab('GIS')}>GIS</button>
-            </div>
-            {filterIcons(selectedTab).map((skill, index) => (
-              <div key={index} className="skills-icon">
-                {skill.skills_icon && (
-                  <a href={skill.skills_icon.url} target="_blank" rel="noopener noreferrer">
-                    <img
-                      src={skill.skills_icon.url}
-                      alt={skill.skills_icon.alt}
-                      width="64px"
-                      height="64px"
-                    />
-                  </a>
-                )}
-              </div>
-            ))}
+          <section className="skills-content">
+            {restData.acf.skills_logo && (
+                <div className="skills-logo">
+                  <img
+                    src={restData.acf.skills_logo.url}
+                    alt={restData.acf.skills_logo.alt}
+                  />
+                </div>
+              )}
+            <article className="skills-intro">
+              <h2>{restData.acf.skills_header}</h2>
+              <div
+                dangerouslySetInnerHTML={{ __html: restData.acf.skills_intro }}
+              />
+            </article>
+            <Tabs>
+              <TabList>
+                <Tab>All</Tab>
+                <Tab>Web Dev</Tab>
+                <Tab>GIS</Tab>
+              </TabList>
+
+              <TabPanel>
+                <section className="skills-icons">
+                  {filterIcons("all").map((skill, index) => (
+                    <div key={index} className="skills-icon">
+                      {skill.skills_icon && (
+                        <a
+                          href={skill.skills_icon.url}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          <img
+                            src={skill.skills_icon.url}
+                            alt={skill.skills_icon.alt}
+                            width="64px"
+                            height="64px"
+                          />
+                        </a>
+                      )}
+                    </div>
+                  ))}
+                </section>
+              </TabPanel>
+
+              <TabPanel>
+                <section className="skills-icons">
+                  {filterIcons("Web Dev").map((skill, index) => (
+                    <div key={index} className="skills-icon">
+                      {skill.skills_icon && (
+                        <a
+                          href={skill.skills_icon.url}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          <img
+                            src={skill.skills_icon.url}
+                            alt={skill.skills_icon.alt}
+                            width="64px"
+                            height="64px"
+                          />
+                        </a>
+                      )}
+                    </div>
+                  ))}
+                </section>
+              </TabPanel>
+
+              <TabPanel>
+                <section className="skills-icons">
+                  {filterIcons("GIS").map((skill, index) => (
+                    <div key={index} className="skills-icon">
+                      {skill.skills_icon && (
+                        <a
+                          href={skill.skills_icon.url}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          <img
+                            src={skill.skills_icon.url}
+                            alt={skill.skills_icon.alt}
+                            width="64px"
+                            height="64px"
+                          />
+                        </a>
+                      )}
+                    </div>
+                  ))}
+                </section>
+              </TabPanel>
+            </Tabs>
           </section>
           <section className="skills-btn-call-to-action">
             {restData.acf.global_buttons && (
-            <GlobalButtons buttons={restData.acf.global_buttons} />
-          )}
+              <GlobalButtons buttons={restData.acf.global_buttons} />
+            )}
           </section>
         </section>
       ) : (
@@ -78,4 +147,4 @@ const Services = () => {
   );
 };
 
-export default Services;
+export default Skills;

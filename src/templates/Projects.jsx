ef1c0f1082e1105cbb63037bd1e7189ebe5f2811
hoornaert-cj <97850176+hoornaert-cj@users.noreply.github.com>
2024-06-23 15:25:30 -0700

    For bigger screens, changed the style of the Project card from portrait to landscape.

--- src/templates/Projects.jsx
+++ src/templates/Projects.jsx
@@ -1,11 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import Loading from '../utilities/Loading';
-import { restBase } from '../utilities/Utilities';
-
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import Loading from "../utilities/Loading";
+import { restBase } from "../utilities/Utilities";
 
 const Projects = () => {
-  const restPath = restBase + 'pages/51';
+  const restPath = restBase + "pages/51";
   const [restData, setRestData] = useState([]);
   const [isLoaded, setLoadStatus] = useState(false);
 
@@ -22,7 +21,7 @@ const Projects = () => {
           setLoadStatus(false);
         }
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
         setLoadStatus(false);
       }
     };
@@ -34,41 +33,41 @@ const Projects = () => {
       {isLoaded ? (
         <section id="introduction" className="projects-wrapper">
           <section className="projects-content">
-
-          <section className='projects-intro'>
-          {restData.acf && restData.acf.projects_heading_image && (
-            <img
-              src={restData.acf.projects_heading_image.url}
-              alt={restData.acf.projects_heading_image.alt}
-            />
-          )}
-            <section className='project-intro-text'>
-            <h1>{restData.title.rendered}</h1>
-            <p>{restData.acf.projects_intro}</p>
+            <section className="projects-intro">
+              {restData.acf && restData.acf.projects_heading_image && (
+                <img
+                  src={restData.acf.projects_heading_image.url}
+                  alt={restData.acf.projects_heading_image.alt}
+                />
+              )}
+              <section className="project-intro-text">
+                <h1>{restData.title.rendered}</h1>
+                <p>{restData.acf.projects_intro}</p>
+              </section>
             </section>
-          </section>
 
-
-          {restData.acf && restData.acf.project_card && (
-            <section className="project-cards">
-              {restData.acf.project_card.map((card, index) => (
-                <div key={index} className="project-card">
-                  {card.project_image && (
-                    <img
-                      src={card.project_image.url}
-                      alt={card.project_image.alt}
-                    />
-                  )}
-                  <h2>{card.project_title}</h2>
-                  <p>{card.project_description}</p>
-                  {/* Modify Link to use project_post_id */}
-                  <section className='projects-btn button'>
-                    <Link to={`/project/${card.project_post_id}`}>Detailed Project Info</Link>
-                  </section>
-                </div>
-              ))}
-            </section>
-          )}
+            {restData.acf && restData.acf.project_card && (
+              <section className="project-cards">
+                {restData.acf.project_card.map((card, index) => (
+                  <div key={index} className="project-card">
+                    {card.project_image && (
+                      <img
+                        src={card.project_image.url}
+                        alt={card.project_image.alt}
+                      />
+                    )}
+                    <h2>{card.project_title}</h2>
+                    <p>{card.project_description}</p>
+                    {/* Modify Link to use project_post_id */}
+                    <section className="projects-btn button">
+                      <Link to={`/project/${card.project_post_id}`}>
+                        Detailed Project Info
+                      </Link>
+                    </section>
+                  </div>
+                ))}
+              </section>
+            )}
           </section>
         </section>
       ) : (
